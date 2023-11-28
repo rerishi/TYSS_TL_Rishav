f83945fa0bc23f91de940797538e1c4bbf9c11a9
rerishi <rishav.rq@gmail.com>
2023-11-28 10:55:20 +0530

    Complete Framework with Two Testscripts

---
+++ src/test/java/sdet52/TestScript12Test.java
@@ -0,0 +1,18 @@
+package sdet52;
+
+import org.testng.annotations.Test;
+
+public class TestScript12Test {
+
+	@Test(groups = "Smoke")
+	public void ts1Test()
+	{
+		System.out.println("----TScript1----");
+	}
+	
+	@Test
+	public void ts2Test()
+	{
+		System.out.println("----TScript2----");
+	}
+}
---
+++ src/test/java/sdet52/TestScript34Test.java
@@ -0,0 +1,18 @@
+package sdet52;
+
+import org.testng.annotations.Test;
+
+public class TestScript34Test {
+
+	@Test(groups = "Smoke")
+	public void ts3Test()
+	{
+		System.out.println("----TScript3----");
+	}
+	
+	@Test(groups = "Regression")
+	public void ts4Test()
+	{
+		System.out.println("----TScript4----");
+	}
+}
