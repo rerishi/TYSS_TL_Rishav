cd84a631892a5e4359375b5c5e57120b646df8ac
rerishi <rishav3220@gmail.com>
2023-11-30 13:22:49 +0530

    Base Test Added

---
+++ src/test/java/SecondPackage/BaseTest.java
@@ -0,0 +1,48 @@
+package SecondPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	 WebDriver driver;
+	
+	@Test
+	public void parameterTest()
+	{
+		String BROWSER=System.getProperty("browser");
+		String URL=System.getProperty("url");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		
+		if(BROWSER.equalsIgnoreCase("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
+		else
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		
+		driver.get(URL);
+	}
+	
+}
