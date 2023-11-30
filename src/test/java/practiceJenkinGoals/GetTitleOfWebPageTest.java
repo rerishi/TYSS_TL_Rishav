583dad968c9c974705317619e15ca7f36ae96b9e
rerishi <rishav3220@gmail.com>
2023-11-30 10:30:57 +0530

    Using Parameter

--- src/test/java/practiceJenkinGoals/GetTitleOfWebPageTest.java
+++ src/test/java/practiceJenkinGoals/GetTitleOfWebPageTest.java
@@ -4,25 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 //import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GetTitleOfWebPageTest {
-//	WebDriver driver;
-//
-//	//@Parameters("BROWSER")
-//	@Test
-//	public void title(String browser) {
-//		if (browser.equalsIgnoreCase("chrome"))
-//
-//			driver = new ChromeDriver();
-//		else if (browser.equalsIgnoreCase("firefox"))
-//			driver = new FirefoxDriver();
-//		else
-//			driver = new EdgeDriver();
-//
-//		driver.get("https://www.google.com/");
-//		String title = driver.getTitle();
-//		System.out.println(title);
-	//}
+	WebDriver driver;
+
+	@Parameters("BROWSER")
+	@Test
+	public void title(String browser) {
+		if (browser.equalsIgnoreCase("chrome"))
+
+			driver = new ChromeDriver();
+		else if (browser.equalsIgnoreCase("firefox"))
+			driver = new FirefoxDriver();
+		else
+			driver = new EdgeDriver();
+
+		driver.get("https://www.google.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
 }
